9cd77cc1ff1968384b6b87e8b99ddc4e0be8618c
Mikylla Cayago <mcacayago@gmail.com>
2026-05-03 17:38:14 +0800

    add: character rotates alongside camera

--- camera_system.gd
+++ camera_system.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
+@export var character: CharacterBody3D
+
 var camera_rotation: Vector2 = Vector2.ZERO
 var mouse_sensitivity: float = 0.001
+var max_y_rotation: float = 1.5
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -20,5 +23,9 @@ func camera_look(mouse_movement : Vector2) -> void:
 	camera_rotation += mouse_movement
 	
 	transform.basis = Basis()
-	rotate_object_local(Vector3(0,1,0), -camera_rotation.x)
-	rotate_object_local(Vector3(1,0,0), -camera_rotation.y)		
+	character.transform.basis = Basis()
+	
+	character.rotate_object_local(Vector3(0,1,0), -camera_rotation.x)
+	character.rotate_object_local(Vector3(1,0,0), -camera_rotation.y)		
+	
+	camera_rotation.y = clamp(camera_rotation.y, -max_y_rotation, max_y_rotation)

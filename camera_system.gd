57397b0948bb3197f812d789308ffee231e1d17e
Mikylla Cayago <mcacayago@gmail.com>
2026-05-03 23:54:14 +0800

    add: camera switch

--- camera_system.gd
+++ camera_system.gd
@@ -1,6 +1,11 @@
 extends Node3D
 
 @export var character: CharacterBody3D
+@export var edge_spring_arm: SpringArm3D
+var camera_alignment_speed: float = 0.2
+
+var camera_tween: Tween
+
 
 var camera_rotation: Vector2 = Vector2.ZERO
 var mouse_sensitivity: float = 0.001
@@ -18,6 +23,14 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var mouse_event: Vector2 = event.screen_relative * mouse_sensitivity
 		camera_look(mouse_event)
+	
+	if event.is_action_pressed("swap_camera_alignment"):
+		swap_camera_alignment()
+	
+	if event.is_action_pressed("aim"):
+		pass
+	
+	
 		
 func camera_look(mouse_movement : Vector2) -> void:
 	camera_rotation += mouse_movement
@@ -29,3 +42,17 @@ func camera_look(mouse_movement : Vector2) -> void:
 	character.rotate_object_local(Vector3(1,0,0), -camera_rotation.y)		
 	
 	camera_rotation.y = clamp(camera_rotation.y, -max_y_rotation, max_y_rotation)
+	
+func enter_aim() -> void:
+	pass
+
+func swap_camera_alignment() -> void:
+	var new_pos: float = -edge_spring_arm.spring_length
+	set_rear_spring_arm_position(new_pos,camera_alignment_speed)
+func set_rear_spring_arm_position(pos: float, speed: float) -> void:
+	if camera_tween: 
+		camera_tween.kill()
+	camera_tween = get_tree().create_tween()
+	camera_tween.tween_property(edge_spring_arm,"spring_length",pos,speed)
+	
+	

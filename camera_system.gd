d9600291c8e7ca6343048226631b96a43e69648b
Mikylla Cayago <mcacayago@gmail.com>
2026-05-03 17:30:15 +0800

    add: camera rotation follows mouse movement

--- camera_system.gd
+++ camera_system.gd
@@ -1,14 +1,10 @@
 extends Node3D
 
-
+var camera_rotation: Vector2 = Vector2.ZERO
+var mouse_sensitivity: float = 0.001
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -16,3 +12,13 @@ func _input(event: InputEvent) -> void:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else: 
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if event is InputEventMouseMotion:
+		var mouse_event: Vector2 = event.screen_relative * mouse_sensitivity
+		camera_look(mouse_event)
+		
+func camera_look(mouse_movement : Vector2) -> void:
+	camera_rotation += mouse_movement
+	
+	transform.basis = Basis()
+	rotate_object_local(Vector3(0,1,0), -camera_rotation.x)
+	rotate_object_local(Vector3(1,0,0), -camera_rotation.y)		

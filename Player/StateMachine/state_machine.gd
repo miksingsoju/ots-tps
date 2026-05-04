227794defa72973fab19127edc2a08386627bd7b
amantenoah <111347849+amantenoah@users.noreply.github.com>
2026-05-04 13:14:10 +0800

    fixed naming bug to prevent crashes

--- Player/StateMachine/state_machine.gd
+++ Player/StateMachine/state_machine.gd
@@ -14,7 +14,7 @@ func _input(event: InputEvent) -> void:
 	current_state._state_input(event)
 	
 func _physics_process(delta: float) -> void:
-	current_state.update(delta)
+	current_state._update(delta)
 
 func _create_state_map() -> void:
 	for child: State in get_children():
@@ -38,4 +38,4 @@ func _change_state(state_name: String) -> void:
 		
 	current_state._exit()
 	current_state = state_map[state_name]
-	
+	current_state._enter()

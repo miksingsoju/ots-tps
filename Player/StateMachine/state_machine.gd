6c51855efeba2c3333bcdbe34520c452056c9bce
amantenoah <111347849+amantenoah@users.noreply.github.com>
2026-05-04 13:09:10 +0800

    adjusted folder structure and created state_machine

---
+++ Player/StateMachine/state_machine.gd
@@ -0,0 +1,41 @@
+extends Node
+class_name StateMachine
+
+@export var start_state: State
+var state_map: Dictionary
+
+var current_state: State = null
+var _active: bool = false
+
+func _ready() -> void:
+	_initialize(start_state)
+
+func _input(event: InputEvent) -> void:
+	current_state._state_input(event)
+	
+func _physics_process(delta: float) -> void:
+	current_state.update(delta)
+
+func _create_state_map() -> void:
+	for child: State in get_children():
+		child.finished.connect(_change_state)
+		state_map[child.name] = child
+
+func _initialize(state: State) -> void:
+	current_state = state
+	current_state._enter()
+	
+func set_active(value: bool) -> void:
+	_active = value
+	set_physics_process(value)
+	set_process_input(value)
+	if not _active:
+		current_state = null
+		
+func _change_state(state_name: String) -> void:
+	if not _active:
+		return
+		
+	current_state._exit()
+	current_state = state_map[state_name]
+	
